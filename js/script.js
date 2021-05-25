fe32e37a89d7e5f852fc55b18aeb2d5caaf8c116
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-05-25 12:06:17 +0200

    V6.0 quelque modification rien d'important

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-// #### Script js en cours ####
+// Formulaire 
 
+let validation = document.getElementById('valid-formulaire');
 
+validation.addEventListener('input', function(event) {
 
+    event.preventDefault();
+    event.stopPropagation();
+    validation.window.confirm('confirmer l\'envoi du message ?');
+    
 
+});
+// confirmation de validation a revoir 
+// si il confirme ca donne true, sinon false
 
 
