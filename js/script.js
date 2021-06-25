764e4323da7ddd0a69d10aa2dda2cdf7bed9487c
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-06-25 09:26:49 +0200

    v7.7 A revoir le responsive et scroll

--- js/script.js
+++ js/script.js
@@ -1,66 +1,66 @@
-/* // Variables - Constantes 
-
-// ####### 1one.scss #######
-
-
-// ####### 2two.scss #######
-
-const aside = document.querySelector('.aside-JS');
-const article = document.querySelector('.article-JS');
-
-// PERMET de récupéré la hauteur de l'element quelque soit ca hauteur, en cas de changement responsive par exemple
-let screen2 = aside.clientHeight;
-// ####### 3three.scss #####
-
-
-// ####### 4four.scss ######
-
-
-// ####### 5five.scss ######
-
-
-// ####### 6six.scss #######
-
-window.addEventListener('scroll', () => {
-
-    if(window.scrollY > 400) {
-
-        aside.classList.add('scroll');
-
-    }
-    else{
-
-        aside.classList.remove('scroll');
-
-    }
-    
-});
-
-window.addEventListener('scroll', () => {
-
-    if(window.scrollY > screen2) {
-
-        aside.classList.add('scroll');
-
-    }
-    else{
-
-        aside.classList.remove('scroll');
-
-    }
-    
-}); 
-
-*/
-
-
-const ratio = .1
+// ratio scroll 25% visible and pop
+const ratio = .25
 
 const options = {
     root: null,
     rootMargin: '0px',
     threshold: ratio
 }
+
+
+
+// REVEAL background
+const backgroundOpacity = function (entries, observer) {
+
+    entries.forEach(function (entry) {
+
+        if (entry.intersectionRatio > ratio) {
+
+            entry.target.classList.add('reveal-visible-background')
+            observer.unobserve(entry.target)
+
+        }
+    })
+}
+
+document.documentElement.classList.add('reveal-loaded')
+window.addEventListener('DOMContentLoaded', function () {
+
+    const observer = new IntersectionObserver(backgroundOpacity, options)
+    document.querySelectorAll('.reveal-background').forEach(function (r) {
+
+        observer.observe(r)
+
+    })
+})
+
+// REVEAL background 2
+const backgroundOpacity2 = function (entries, observer) {
+
+    entries.forEach(function (entry) {
+
+        if (entry.intersectionRatio > ratio) {
+
+            entry.target.classList.add('reveal-visible-background-2')
+            observer.unobserve(entry.target)
+
+        }
+    })
+}
+
+document.documentElement.classList.add('reveal-loaded')
+window.addEventListener('DOMContentLoaded', function () {
+
+    const observer = new IntersectionObserver(backgroundOpacity2, options)
+    document.querySelectorAll('.reveal-background-2').forEach(function (r) {
+
+        observer.observe(r)
+
+    })
+})
+
+
+
 
 
 // REVEAL LEFT
@@ -88,10 +88,6 @@ window.addEventListener('DOMContentLoaded', function () {
     })
 })
 
-
-
-
-
 // REVEAL RIGHT
 const myScrollRight = function (entries, observer) {
 
@@ -116,6 +112,403 @@ window.addEventListener('DOMContentLoaded', function () {
 
     })
 })
+
+
+
+
+
+
+// REVEAL TITLE LEFT
+const myScrollLeftTitle = function (entries, observer) {
+
+    entries.forEach(function (entry) {
+
+        if (entry.intersectionRatio > ratio) {
+
+            entry.target.classList.add('reveal-visible-L--title')
+            observer.unobserve(entry.target)
+
+        }
+    })
+}
+
+document.documentElement.classList.add('reveal-loaded')
+window.addEventListener('DOMContentLoaded', function () {
+
+    const observer = new IntersectionObserver(myScrollLeftTitle, options)
+    document.querySelectorAll('.reveal-L--title').forEach(function (r) {
+
+        observer.observe(r)
+
+    })
+})
+
+// REVEAL TITLE RIGHT
+const myScrollRightTitle = function (entries, observer) {
+
+    entries.forEach(function (entry) {
+
+        if (entry.intersectionRatio > ratio) {
+
+            entry.target.classList.add('reveal-visible-R--title')
+            observer.unobserve(entry.target)
+
+        }
+    })
+}
+
+document.documentElement.classList.add('reveal-loaded')
+window.addEventListener('DOMContentLoaded', function () {
+
+    const observer = new IntersectionObserver(myScrollRightTitle, options)
+    document.querySelectorAll('.reveal-R--title').forEach(function (r) {
+
+        observer.observe(r)
+
+    })
+})
+
+
+
+
+
+
+
+
+// REVEAL TITLE LEFT 2
+const myScrollLeftTitle2 = function (entries, observer) {
+
+    entries.forEach(function (entry) {
+
+        if (entry.intersectionRatio > ratio) {
+
+            entry.target.classList.add('reveal-visible-L--title-2')
+            observer.unobserve(entry.target)
+
+        }
+    })
+}
+
+document.documentElement.classList.add('reveal-loaded')
+window.addEventListener('DOMContentLoaded', function () {
+
+    const observer = new IntersectionObserver(myScrollLeftTitle2, options)
+    document.querySelectorAll('.reveal-L--title-2').forEach(function (r) {
+
+        observer.observe(r)
+
+    })
+})
+
+// REVEAL TITLE RIGHT 2
+const myScrollRightTitle2 = function (entries, observer) {
+
+    entries.forEach(function (entry) {
+
+        if (entry.intersectionRatio > ratio) {
+
+            entry.target.classList.add('reveal-visible-R--title-2')
+            observer.unobserve(entry.target)
+
+        }
+    })
+}
+
+document.documentElement.classList.add('reveal-loaded')
+window.addEventListener('DOMContentLoaded', function () {
+
+    const observer = new IntersectionObserver(myScrollRightTitle2, options)
+    document.querySelectorAll('.reveal-R--title-2').forEach(function (r) {
+
+        observer.observe(r)
+
+    })
+})
+
+
+
+
+
+
+
+
+// REVEAL TITLE LEFT 3
+const myScrollLeftTitle3 = function (entries, observer) {
+
+    entries.forEach(function (entry) {
+
+        if (entry.intersectionRatio > ratio) {
+
+            entry.target.classList.add('reveal-visible-L--title-3')
+            observer.unobserve(entry.target)
+
+        }
+    })
+}
+
+document.documentElement.classList.add('reveal-loaded')
+window.addEventListener('DOMContentLoaded', function () {
+
+    const observer = new IntersectionObserver(myScrollLeftTitle3, options)
+    document.querySelectorAll('.reveal-L--title-3').forEach(function (r) {
+
+        observer.observe(r)
+
+    })
+})
+
+// REVEAL TITLE RIGHT 3
+const myScrollRightTitle3 = function (entries, observer) {
+
+    entries.forEach(function (entry) {
+
+        if (entry.intersectionRatio > ratio) {
+
+            entry.target.classList.add('reveal-visible-R--title-3')
+            observer.unobserve(entry.target)
+
+        }
+    })
+}
+
+document.documentElement.classList.add('reveal-loaded')
+window.addEventListener('DOMContentLoaded', function () {
+
+    const observer = new IntersectionObserver(myScrollRightTitle3, options)
+    document.querySelectorAll('.reveal-R--title-3').forEach(function (r) {
+
+        observer.observe(r)
+
+    })
+})
+
+
+
+
+
+
+
+
+
+
+// REVEAL TITLE LEFT 4
+const myScrollLeftTitle4 = function (entries, observer) {
+
+    entries.forEach(function (entry) {
+
+        if (entry.intersectionRatio > ratio) {
+
+            entry.target.classList.add('reveal-visible-L--title-4')
+            observer.unobserve(entry.target)
+
+        }
+    })
+}
+
+document.documentElement.classList.add('reveal-loaded')
+window.addEventListener('DOMContentLoaded', function () {
+
+    const observer = new IntersectionObserver(myScrollLeftTitle4, options)
+    document.querySelectorAll('.reveal-L--title-4').forEach(function (r) {
+
+        observer.observe(r)
+
+    })
+})
+
+// REVEAL TITLE RIGHT 4
+const myScrollRightTitle4 = function (entries, observer) {
+
+    entries.forEach(function (entry) {
+
+        if (entry.intersectionRatio > ratio) {
+
+            entry.target.classList.add('reveal-visible-R--title-4')
+            observer.unobserve(entry.target)
+
+        }
+    })
+}
+
+document.documentElement.classList.add('reveal-loaded')
+window.addEventListener('DOMContentLoaded', function () {
+
+    const observer = new IntersectionObserver(myScrollRightTitle4, options)
+    document.querySelectorAll('.reveal-R--title-4').forEach(function (r) {
+
+        observer.observe(r)
+
+    })
+})
+
+
+
+
+
+
+
+
+
+// REVEAL POP
+const myScrollPOP = function (entries, observer) {
+
+    entries.forEach(function (entry) {
+
+        if (entry.intersectionRatio > ratio) {
+
+            entry.target.classList.add('reveal-visible-POP')
+            observer.unobserve(entry.target)
+
+        }
+    })
+}
+
+document.documentElement.classList.add('reveal-loaded')
+window.addEventListener('DOMContentLoaded', function () {
+
+    const observer = new IntersectionObserver(myScrollPOP, options)
+    document.querySelectorAll('.reveal-POP').forEach(function (r) {
+
+        observer.observe(r)
+
+    })
+})
+
+// REVEAL POP 2
+const myScrollPOP2 = function (entries, observer) {
+
+    entries.forEach(function (entry) {
+
+        if (entry.intersectionRatio > ratio) {
+
+            entry.target.classList.add('reveal-visible-POP-2')
+            observer.unobserve(entry.target)
+
+        }
+    })
+}
+
+document.documentElement.classList.add('reveal-loaded')
+window.addEventListener('DOMContentLoaded', function () {
+
+    const observer = new IntersectionObserver(myScrollPOP2, options)
+    document.querySelectorAll('.reveal-POP-2').forEach(function (r) {
+
+        observer.observe(r)
+
+    })
+})
+
+
+
+
+
+
+
+
+// Effect light SOCIAL NETWORK 
+
+// light 01
+const light01 = function (entries, observer) {
+
+    entries.forEach(function (entry) {
+
+        if (entry.intersectionRatio > ratio) {
+
+            entry.target.classList.add('effectLight01-visible')
+            observer.unobserve(entry.target)
+
+        }
+    })
+}
+
+document.documentElement.classList.add('reveal-loaded')
+window.addEventListener('DOMContentLoaded', function () {
+
+    const observer = new IntersectionObserver(light01, options)
+    document.querySelectorAll('.effectLight01').forEach(function (r) {
+
+        observer.observe(r)
+
+    })
+})
+
+// light 02
+const light02 = function (entries, observer) {
+
+    entries.forEach(function (entry) {
+
+        if (entry.intersectionRatio > ratio) {
+
+            entry.target.classList.add('effectLight02-visible')
+            observer.unobserve(entry.target)
+
+        }
+    })
+}
+
+document.documentElement.classList.add('reveal-loaded')
+window.addEventListener('DOMContentLoaded', function () {
+
+    const observer = new IntersectionObserver(light02, options)
+    document.querySelectorAll('.effectLight02').forEach(function (r) {
+
+        observer.observe(r)
+
+    })
+})
+
+// light 03
+const light03 = function (entries, observer) {
+
+    entries.forEach(function (entry) {
+
+        if (entry.intersectionRatio > ratio) {
+
+            entry.target.classList.add('effectLight03-visible')
+            observer.unobserve(entry.target)
+
+        }
+    })
+}
+
+document.documentElement.classList.add('reveal-loaded')
+window.addEventListener('DOMContentLoaded', function () {
+
+    const observer = new IntersectionObserver(light03, options)
+    document.querySelectorAll('.effectLight03').forEach(function (r) {
+
+        observer.observe(r)
+
+    })
+})
+
+// light 04 
+const light04 = function (entries, observer) {
+
+    entries.forEach(function (entry) {
+
+        if (entry.intersectionRatio > ratio) {
+
+            entry.target.classList.add('effectLight04-visible')
+            observer.unobserve(entry.target)
+
+        }
+    })
+}
+
+document.documentElement.classList.add('reveal-loaded')
+window.addEventListener('DOMContentLoaded', function () {
+
+    const observer = new IntersectionObserver(light04, options)
+    document.querySelectorAll('.effectLight04').forEach(function (r) {
+
+        observer.observe(r)
+
+    })
+})
+
+
 
 
 
